e188215d00ae4b35daa736203d5212424837ed90
Luis-Mej <luis.mejia@tecnologicoliceocristiano.edu.ec>
2025-06-19 19:39:49 -0500

    Add files via upload

---
+++ Extensiones/Extensions/Extend.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensiones.Extensions
+{
+    public static class Extend
+    {
+        public static string GenerarNombreUsuario(this string nombresCompletos)
+        {
+            var nombres = nombresCompletos.Split(' ');
+
+            if (nombres.Length > 2)
+            {
+                throw new ArgumentException("El nombre completo debe contener solo un nombre y un apellido.");
+            }
+
+            return nombres[0].Substring(0, 1).ToUpper() + nombres[1].ToUpper();
+        }
+    }
+}
